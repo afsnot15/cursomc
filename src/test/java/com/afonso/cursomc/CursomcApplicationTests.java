33b1b740d593cc44d9a85ee2af41369192b68c60
Afonso Gomes <afonso.gomes@vrsoft.com.br>
2020-10-12 12:58:28 -0300

    Criando Projeto

---
+++ src/test/java/com/afonso/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afonso.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
