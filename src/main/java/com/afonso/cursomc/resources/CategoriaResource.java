02597400f49f617f0327a2d21b59ed664bd70472
Afonso Gomes <afonso.gomes@vrsoft.com.br>
2020-10-12 19:36:58 -0300

    Corrigindo importacao

--- src/main/java/com/afonso/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/afonso/cursomc/resources/CategoriaResource.java
@@ -2,8 +2,6 @@ package com.afonso.cursomc.resources;
 
 import com.afonso.cursomc.domain.Categoria;
 import com.afonso.cursomc.services.CategoriaService;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
