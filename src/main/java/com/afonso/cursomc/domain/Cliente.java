b323e626be8b9e86977fdfac117dce5b95280ce5
Afonso Gomes <afsnot15@gmail.com>
2021-01-04 15:25:25 -0300

    Ajustando e testando delecao de cliente

--- src/main/java/com/afonso/cursomc/domain/Cliente.java
+++ src/main/java/com/afonso/cursomc/domain/Cliente.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -27,7 +28,7 @@ public class Cliente implements Serializable {
     private String cpfCnpj;
     private Integer tipoCliente;
 
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(mappedBy = "cliente", cascade=CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
     @ElementCollection
